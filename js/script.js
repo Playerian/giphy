1a5e1043cbb4e470bd7e095867ae0514a4d9a14a
Liwei Huang <aa583045912@gmail.com>
2019-02-27 01:47:28 +0000

    pretty making

--- js/script.js
+++ js/script.js
@@ -2,10 +2,51 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
+//https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC
+let query = '';
+let offset = 0;
 
 $("#search-button").click(function(){
-  
-  
-  
+  query = $("#search-term").val();
+  $.ajax({
+    url: `https://api.giphy.com/v1/gifs/search?q=${query}&rating=pg&api_key=dc6zaTOxFJmzC&offset=${offset}`,
+    method: "GET", 
+    success: (res) => {
+        console.log(res);
+        $(".gallery").empty();
+        let gifs = res.data;
+        render(gifs);
+    }
+  });
 });
 
+$(window).scroll(function() {
+    console.log('run');
+    if (query === ''){
+        return;
+    }
+    if($(window).scrollTop() + $(window).height() == $(document).height()) {
+        window.scrollBy(0, -1);
+        offset += 25;
+        $.ajax({
+        url: `https://api.giphy.com/v1/gifs/search?q=${query}&offset=${offset}&rating=pg&api_key=dc6zaTOxFJmzC`,
+        method: "GET", 
+        success: (res) => {
+            let gifs = res.data;
+            render(gifs);
+        }
+      });
+    }
+});
+
+function render(gifs){
+    for (let i = 0; i < gifs.length; i++){
+        let gif = gifs[i];
+        let image = gif.images.original;
+        let $gif = $('<img>').addClass('image');
+        $gif.css('width', image.width);
+        $gif.css('height', image.height);
+        $gif.attr('src', image.url)
+        $('.gallery').append($gif);
+    }
+}
